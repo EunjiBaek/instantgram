039e0a3ae7501c8627b2cb779da7b1276cc424bf
HANSAE-MIS <mis@hansae.com>
2024-03-06 11:53:58 +0700

    commit

---
+++ src/components/CommentForm.tsx
@@ -0,0 +1,34 @@
+import { FormEvent, useState } from 'react';
+import SmileIcon from './ui/icons/SmileIcon';
+
+type Props = {
+  onPostComment: (comment: string) => void;
+}
+
+export default function CommentForm({onPostComment}: Props) {
+
+  const [comment, setComment] = useState('');
+  const buttonDisabled = comment.length === 0;
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    onPostComment(comment);
+    setComment('');
+  }
+
+
+  return (
+    <form onSubmit={handleSubmit} className='flex items-center px-3 border-t border-neutral-300'>
+      <SmileIcon />
+      <input
+        className='w-full p-3 ml-2 border-none outline-none'
+        type='text'
+        placeholder='Add a comment...'
+        required
+        value={comment}
+        onChange={e => setComment(e.target.value)}
+      />
+      <button disabled={buttonDisabled} className={`ml-2 font-bold ${buttonDisabled ? 'text-sky-300' : 'text-sky-500'}`}>Post</button>
+    </form>
+  );
+}
---
+++ src/components/PostDetail.tsx
@@ -0,0 +1,55 @@
+import { SimplePost } from '@/model/post';
+import Image from 'next/image';
+import ActionBar from './ActionBar';
+import Avatar from './Avatar';
+import PostUserAvatar from './PostUserAvatar';
+import useFullPost from '@/hooks/post';
+
+type Props = {
+  post: SimplePost;
+};
+
+export default function PostDetail({ post }: Props) {
+  const { id, userImage, username, image } = post;
+  const {post: data, postComment} = useFullPost(id);
+  const comments = data?.comments;
+
+
+  return (
+    <section className='flex w-full h-full'>
+      <div className='relative basis-3/5'>
+        <Image
+          className='object-cover'
+          src={image}
+          alt={`photo by ${username}`}
+          priority
+          fill
+          sizes='650px'
+        />
+      </div>
+      <div className='flex flex-col w-full basis-2/5'>
+        <PostUserAvatar image={userImage} username={username} />
+        <ul className='h-full p-4 mb-1 overflow-y-auto border-t border-gray-200'>
+          {comments &&
+            comments.map(
+              ({ image, username: commentUsername, comment }, index) => (
+                <li key={index} className='flex items-center mb-1'>
+                  <Avatar
+                    image={image}
+                    size='small'
+                    highlight={commentUsername === username}
+                  />
+                  <div className='ml-2'>
+                    <span className='mr-1 font-bold'>{commentUsername}</span>
+                    <span>{comment}</span>
+                  </div>
+                </li>
+              )
+            )}
+        </ul>
+        <ActionBar post={post} onComment={postComment} />
+
+      </div>
+    </section>
+  );
+}
